15f9c27d4236d50c4454c29412f935ce73cd4cce
Ben <kyutheoriginal@gmail.com>
2022-07-11 08:23:55 +0200

    readme

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ const Modal = (props) => {
     return(
             <div className="modal" onClick={() => props.closeModal(false)}>
                 <div className="modal-box"> <div className="exit">X</div>
-                    <img src={`http://covers.openlibrary.org/b/id/${props.id}-L.jpg`}></img>   
+                    <img src={`http://covers.openlibrary.org/b/id/${props.id}-L.jpg`} alt="enhanced modal"></img>   
                 </div>              
         </div>
     )
